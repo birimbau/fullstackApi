ca9c5893cabd6990f8939fe988f840ed7d4aa8e8
birimbau <tiagompbneto@gmail.com>
2020-05-02 23:39:16 +0200

    Heroku preparations

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-var morgan = require('morgan');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(
@@ -105,7 +107,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
